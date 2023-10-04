419cdda570bf9fafb29ac72d5334ed30a9b3ae28
Imane <Imane@MacBook-Air-de-PayeTaPinte.local>
2023-10-04 16:33:36 +0200

    exercice algo JS

---
+++ exercice/script.js
@@ -0,0 +1,42 @@
+ 
+
+ //6.1__________________________
+
+/* let tableau = Array(7).fill(0);
+
+for (let i = 0; i < 7; i++) {
+    console.log(tableau[i]);
+}
+ */
+
+ //6.2_________________________
+
+/* let voyelles = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+for (let i = 0; i < voyelles.length; i++) {
+    console.log(voyelles[i]); 
+}*/
+
+//6.3____________________________
+
+/* let note = Array(9)
+
+
+for (let i = 0; i < note.length; i++){
+    note[i] = prompt(`note ${i} `)
+    console.log(" la note est" , note[i+1]);
+    
+}
+ console.log(note.join( ",")); */
+
+//6.4____________________________
+
+/* let Nb = new Array(6);
+
+
+for (let i = 0; i < 6; i++) {
+    Nb[i] = i * i;
+    console.log(Nb[i]);
+} */
+
+//6.5____________________________
